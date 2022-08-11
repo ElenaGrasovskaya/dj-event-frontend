08fb7cd9b857fe378a752fc48ed52c659a4ab81e
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2022-08-11 23:45:47 +0300

    Enter button bug fixed

--- components/Items.js
+++ components/Items.js
@@ -1,85 +1,114 @@
-import styles from '@/styles/Items.module.css';
-import {useState} from 'react';
+import styles from "@/styles/Items.module.css";
+import { useState } from "react";
 
 import { BsXCircle } from "react-icons/bs";
 
-function Items({items, callback}) {
+function Items({ items, callback }) {
+  const [expense, setExpense] = useState({
+    items: [
+      ...items.items,
+      { name: "", price: 0, description: "", status: false },
+    ],
+  });
 
-      
-    const [expense, setExpense] = useState({items: [...items.items, {name:"", price:0, description:"", status:false}]})
+  const handleInputChange = (e, index) => {
+    let { name, value, checked } = e.target;
 
-    const handleInputChange = (e, index) => {
+    let newItems;
 
-            let { name, value, checked } = e.target;
-
-            let newItems;
-            
-            if(name==="status")
-            {
-                newItems  = expense.items.map((item, i)=>{
-                    if (i===index)
-                    {
-                        return {...item, status:checked}
-                    }
-                    else return item;
-                })
-           
-          
-            }
-            else{
-              
-                    
-                 newItems = expense.items.map((item, i)=>{
-                if (i===index)
-                {
-                    return {...item, [name]:value}
-                }
-                else return item;
-            })
-
-            }
-            
-
-            if (index===newItems.length-1)
-            {
-                newItems = [...newItems, {name:"", price:0, description:"", status:false}]
-
-           }  
-    
-
-
-            setExpense({items:newItems});
-            callback({target: {
-                name: "items",
-                value:{items:newItems},
-            }});
-            const newExpense = expense.items.reduce((acc, i)=>{return acc+Number(i.price)},0);
-    
+    if (name === "status") {
+      newItems = expense.items.map((item, i) => {
+        if (i === index) {
+          return { ...item, status: checked };
+        } else return item;
+      });
+    } else {
+      newItems = expense.items.map((item, i) => {
+        if (i === index) {
+          return { ...item, [name]: value };
+        } else return item;
+      });
     }
 
-    const handleDelete = (e, index) => {
+    if (index === newItems.length - 1) {
+      newItems = [
+        ...newItems,
+        { name: "", price: 0, description: "", status: false },
+      ];
+    }
+
+    setExpense({ items: newItems });
+    callback({
+      target: {
+        name: "items",
+        value: { items: newItems },
+      },
+    });
+    const newExpense = expense.items.reduce((acc, i) => {
+      return acc + Number(i.price);
+    }, 0);
+  };
+  const handleKeyPress = (e, index) => {
+    if( e.key === "Enter")
+    {
         e.preventDefault();
-        const newItems = expense.items.filter((el, i)=>i!==index);
-
-        setExpense({items:newItems});
-        callback({target: {
-            name: "items",
-            value:{items:newItems},
-        }});
-
+        handleInputChange(e, index);
     }
 
-    
-    return (
-        <div className={styles.itemsBlock}>
-             {expense.items.map((item, index)=>( <div className={styles.itemBlock} key={index+100}>
-            <input type='text' id={'item'+index} name='name' value={item.name} onChange={(e)=> handleInputChange(e, index)} />
-            <input type='number' id={'item'+index} name='price' value={item.price} onChange={(e)=> handleInputChange(e, index)} />
-            <div className={styles.buttonBlock}><button className={styles.deleteButton} onClick={(e)=>handleDelete(e,index)}><BsXCircle/></button>
-            <input className={styles.statusCheck} type='checkbox' id={'item'+index} name='status' checked={item.status} onChange={(e)=> handleInputChange(e, index)} /></div></div>))}
-            
+  }
+  const handleDelete = (e, index) => {
+    e.preventDefault();
+    const newItems = expense.items.filter((el, i) => i !== index);
+
+    setExpense({ items: newItems });
+    callback({
+      target: {
+        name: "items",
+        value: { items: newItems },
+      },
+    });
+  };
+
+  return (
+    <div className={styles.itemsBlock}>
+      {expense.items.map((item, index) => (
+        <div className={styles.itemBlock} key={index + 100}>
+          <input
+            type="text"
+            id={"item" + index}
+            name="name"
+            value={item.name}
+            onKeyPress={(e)=>handleKeyPress(e, index)}
+            onChange={(e) => handleInputChange(e, index)}
+          />
+          <input
+            type="number"
+            id={"item" + index}
+            name="price"
+            value={item.price}
+            onKeyPress={(e)=>handleKeyPress(e, index)}
+            onChange={(e) => handleInputChange(e, index)}
+          />
+          <div className={styles.buttonBlock}>
+            <button
+              className={styles.deleteButton}
+              onClick={(e) => handleDelete(e, index)}
+            >
+              <BsXCircle />
+            </button>
+            <input
+              className={styles.statusCheck}
+              type="checkbox"
+              id={"item" + index}
+              name="status"
+              checked={item.status}
+              onChange={(e) => handleInputChange(e, index)}
+            />
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Items;
