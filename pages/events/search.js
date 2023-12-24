ad918eaa45bb6386ddc5dcefc4792606b9ee6326
elenagrasovskaya <elenagrasovskaya@gmail.com>
2023-12-25 00:38:48 +0200

    Search fixes

--- pages/events/search.js
+++ pages/events/search.js
@@ -1,32 +1,36 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import {useRouter} from 'next/router'
-import Layout from '@/components/Layout'
-import {API_URL} from '@/config/index'
-import EventItem from '@/components/EventItem'
-import Link from 'next/link'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '@/styles/Home.module.css';
+import { useRouter } from 'next/router';
+import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import EventItem from '@/components/EventItem';
+import Link from 'next/link';
 
-export default function SearchPage({events}) {
-    const router = useRouter()
+export default function SearchPage({ events }) {
+  const router = useRouter();
+  console.log(router.query);
+  console.log("data", events.data);
   return (
     <Layout title='Search Results'>
-        <Link href='/events'>Назад</Link>
+      <Link href='/events'>Назад</Link>
       <h1>Искали {router.query.term}</h1>
 
       {events.length === 0 && <h3>Не нашли</h3>}
-      {events.data.map((evt) => (<EventItem key={evt.id} evt={evt.attributes}/>))}
-      
+      {events.data.map((evt) => (
+        <EventItem key={evt.id} evt={evt.attributes} />
+      ))}
     </Layout>
-  )
+  );
 }
 
-export async function getServerSideProps ({query:term}) {
-      const res = await fetch(`${API_URL}/api/events?filters[title][$containsi]=${term}`)
-  const events = await res.json()
+export async function getServerSideProps({ query: term }) {
+  const res = await fetch(
+    `${API_URL}/api/events?filters[title][$containsi]=${term}`
+  );
+  const events = await res.json();
 
   return {
     props: { events },
-  }
+  };
 }
- 
